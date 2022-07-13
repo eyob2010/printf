2d6acde8a4ff312e9e3b46a44b4ce18f76d158e1
Eyob <e.mersha@gmail.com>
2022-07-13 11:20:37 +0000

    task 0

--- _printf.c
+++ _printf.c
@@ -1,13 +1,37 @@
 #include"stdlib.h"
 #include"stdio.h"
 #include"main.h"
+#include"stdarg.h"
 int _printf(const char *format, ...)
 {
-	while (*format != '\0')
+	char *traverse;
+	unsigned int i;
+	char *s;
+
+	va_list arg;
+	va_start(arg, format);
+
+	for (traverse = format; *traverse != '\0'; traverse++)
 	{
-		_putchar(*format);
-		*format++;
+		while (*traverse != '%')
+		{
+			_putchar(*traverse);
+			traverse++;
+		}
+		traverse++;
+		switch (*traverse)
+		{
+			case 'c' : i = va_arg(arg, int);
+				   _putchar(i);
+				   break;
+			case 's' : s = va_arg(arg, char *);
+				   puts (s);
+				   break;
+		}
 	}
+	va_end(arg);
+
+
 }
 
 int _putchar(char *c)
